544d6da7a692b456dc803bf81301ce14435f306b
Simon <sej@needrent.dk>
2020-11-26 15:12:43 +0100

    Connected POD JSON data and tested connection

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,44 @@
 "use strict";
+/******
+    Variables
+******/
+const linkProdukt = "http://kontrolpanel.johannesantiksalg.dk/wp-json/wp/v2/produkt?_embed";
+const linkInfo = "http://kontrolpanel.johannesantiksalg.dk/wp-json/wp/v2/information";
+const linkEmployees = "http://kontrolpanel.johannesantiksalg.dk/wp-json/wp/v2/medarbejder?_embed";
 
-console.log("hello world");
+/******
+    Fetch data
+******/
+function loadProducts(link) {
+    fetch(link).then(e => e.json()).then(data => showProdukts(data));
+}
+function loadInfo(link) {
+    fetch(link).then(e => e.json()).then(data => showInfo(data));
+}
+function loadEmployees(link) {
+    fetch(link).then(e => e.json()).then(data => showEmployees(data));
+}
+
+/******
+    Use data (funtions)
+******/
+function showProdukts(data){
+    data.forEach(produkt => {
+        console.log(produkt);
+    });
+}
+function showInfo(data){
+        console.log(data);
+}
+function showEmployees(data){
+    data.forEach(employee => {
+        console.log(employee);
+    });
+}
+
+/******
+    Run fetch links
+******/
+loadProducts(linkProdukt);
+loadInfo(linkInfo);
+loadEmployees(linkEmployees);
