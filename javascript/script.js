8285ae3389529ff2371dda1a26781624f110bf79
Simon <sej@needrent.dk>
2020-11-26 17:13:31 +0100

    JSON data ready for DOM

--- javascript/script.js
+++ javascript/script.js
@@ -25,14 +25,72 @@ function loadEmployees(link) {
 function showProdukts(data){
     data.forEach(produkt => {
         console.log(produkt);
+
+         // Image
+         console.log(produkt._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url);
+         console.log(produkt._embedded["wp:featuredmedia"][0].alt_text);
+        
+        //Modal content
+        console.log("MODALE");
+        console.log(produkt.content.rendered);
+        console.log(produkt.ig_link);
+
+        // Vare information
+        const wholeCatalogInfo =  produkt.vare_information.split("\n");
+
+            //insert designer to list (Vare information)
+            wholeCatalogInfo.push(produkt._embedded["wp:term"][0][0].name)
+
+            wholeCatalogInfo.forEach(catalogInfo => {
+                console.log("<li>" + catalogInfo + "</li>");         
+            });
+
+
+            // Price
+            console.log(parseInt(produkt.price));
+            if(produkt.alt_price != "")
+            {
+                console.log(parseInt(produkt.price) + parseInt(produkt.alt_price));
+                console.log(produkt.extra);
+            }
     });
 }
 function showInfo(data){
-        console.log(data);
+        // set up data for HTML
+        console.log(data[0].facetime);
+        console.log(data[0].phone);
+        console.log(data[0].cvr);
+
+        // Split adress Farevejle to two lines
+        const adressFarevejle =  data[0].farevejle_adress.split(", ");
+        console.log(adressFarevejle[0]);
+        console.log(adressFarevejle[1]);
+
+        // Split adress Gislinge to two lines
+        const adressGislinge =  data[0].gislinge_adress.split(", ");
+        console.log(adressGislinge[0]);
+        console.log(adressGislinge[1]);
+
+        // Openings Hours
+        console.log(data[0].open_days01);
+        console.log(data[0].open_hours01a + " – " + data[0].open_hours01b);
+        if(data[0].open_days02 != "")
+            {
+                console.log(data[0].open_days02);
+                console.log(data[0].open_hours02a + " – " + data[0].open_hours02b);
+            }
+
 }
 function showEmployees(data){
     data.forEach(employee => {
         console.log(employee);
+        // About
+        console.log(employee.title.rendered);
+        console.log(employee.content.rendered);
+
+        // Image
+        console.log(employee._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url);
+        console.log(employee._embedded["wp:featuredmedia"][0].alt_text);
     });
 }
 
@@ -42,3 +100,4 @@ function showEmployees(data){
 loadProducts(linkProdukt);
 loadInfo(linkInfo);
 loadEmployees(linkEmployees);
+
